da71e93582725bc2ed0720edcb1b927be6a7634c
Leo Sauzza <leito.akd@gmail.com>
2023-02-11 01:30:08 -0300

    nav-bar now get links from router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '',redirectTo: '/home-page', pathMatch: 'full'},
-  { path: 'home-page', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) }, 
-  { path: 'students', loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) }, 
-  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }];
+  { path: 'home-page',  data: { title: 'Home' }, loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) }, 
+  { path: 'students', data: { title: 'Students' }, loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) }, 
+  { path: 'courses',  data: { title: 'Courses' }, loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { NavLink } from '../../models/nav-link.interface';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-  links = [    
-    { label: 'Home', path: '/home' },
-    { label: 'Students', path: '/students' },
-    { label: 'Courses', path: '/courses' },
-  ];
+export class NavBarComponent implements OnInit{
+  links: NavLink[] = [];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const routes: Routes = this.router.config;
+    this.links = this.router.config.filter(route => route.path)
+    .map(route => ({ label: route.data?.['title'], path: route.path }));
+  }
 }
